af564b78fb8607ceae3ff146b902e01281a111bd
Alfino <nikitamalashkin73@gmail.com>
2023-10-04 11:11:45 +0600

    updated favicon

--- main.jsx
+++ main.jsx
@@ -1,8 +1,8 @@
 import React, { StrictMode } from 'react'
-import App from '@App.jsx'
+import App from 'App.jsx'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import '@styles/index.scss'
+import '@app/styles/index.scss'
 import store from '@store/index.js'
 import { Provider } from 'react-redux'
 
